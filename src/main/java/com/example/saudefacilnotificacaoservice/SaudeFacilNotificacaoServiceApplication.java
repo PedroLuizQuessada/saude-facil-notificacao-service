3fde55ea47c7dcd684dc38229e55f8889e785d07
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-10-07 10:49:31 -0300

    criando projeto

---
+++ src/main/java/com/example/saudefacilnotificacaoservice/SaudeFacilNotificacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.saudefacilnotificacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeFacilNotificacaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaudeFacilNotificacaoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saudefacilnotificacaoservice/SaudeFacilNotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saudefacilnotificacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeFacilNotificacaoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
